2193051aae14c83e1e995ac90970ad144b161f2f
dbittle <dabi9710@colorado.edu>
2014-09-17 14:40:30 -0600

    change 3

--- src/hello/java/hello.java
+++ src/hello/java/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");//display the string
 		System.out.println("How are you?");
-		System.out.println("Stuff hjehaoirwiaw stuff");
+		System.out.println("Stuff stuff");
 		
 	}
 
