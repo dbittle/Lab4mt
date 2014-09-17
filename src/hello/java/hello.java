b6e8ca9a6560f99cd71ba848b0adc4663fc5f173
dbittle <dabi9710@colorado.edu>
2014-09-17 14:00:17 -0600

    Changed commit

--- src/hello/java/hello.java
+++ src/hello/java/hello.java
@@ -4,7 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");//display the string
-		// TODO Auto-generated method stub
+		System.out.println("How are you?");
+		
 	}
 
 }
