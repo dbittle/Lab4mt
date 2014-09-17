1509cef239c8c476f431abe5c9510c24c386451c
dbittle <dabi9710@colorado.edu>
2014-09-17 13:31:45 -0600

    Lab5MT

---
+++ src/hello/java/hello.java
@@ -0,0 +1,10 @@
+package hello.java;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");//display the string
+		// TODO Auto-generated method stub
+	}
+
+}
